abb96c46da421fed5aedc1592bce115237de1e91
Ray Yeung <raymond.zj.yeung@gmail.com>
2018-05-23 12:46:46 -0700

    added Dewey Decimal Index explanation

--- linear-binary-search/src/App.js
+++ linear-binary-search/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import LinearSearch from './linearSearch';
+import Search from './Search';
 import data from './data';
 import sortedData from './sorted-data';
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <LinearSearch 
+        <Search 
           data={data}
           sortedData={sortedData}  
         />
--- linear-binary-search/src/Search.js
+++ linear-binary-search/src/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class LinearSearch extends React.Component {
+export default class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -95,6 +95,31 @@ export default class LinearSearch extends React.Component {
             <p>Binary Search: {this.state.binaryTries}</p>
           </div>
         </form>
+
+        <div>
+          <h4>Dewey Decimal Index</h4>
+          <code>
+              {
+                `const library = [{ author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
+                { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
+                { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
+                { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
+                { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
+                { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
+                { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
+                { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
+                { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane's Fighting Ships' },
+                { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }];`
+              }
+            </code>
+          <p>With a libary using a Dewey Decimal Index like the one above, the process of searching the library would go as follows:</p>
+          <ol>
+            <li>In the library above, the DD index has a decimal and the number values after the decimal point have differen ranges.</li>
+            <li>Because of this, we would need run two seperate searches for the front decimal value and the value after the decimal. We woud use Binary search as it requires less iterations.</li>
+            <li>After running the first search for the front value, we would narrow down the library down to a smaller batch to find our book</li>
+            <li>We then take the batch of matching books with the same front value and run another binary search for the value after the decimal until we find an exact DD index for our book</li>
+          </ol>
+        </div>
       </div>
     );
   }
